00ed59db0222334fcc7b4b9e014e955c536b374c
Victor Chepkasov <victorsugary@gmail.com>
2025-01-06 21:49:33 +0500

    new gen

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+
+	proto "github.com/VictorChepkasov/go-messenger/gen"
+	"github.com/VictorChepkasov/go-messenger/handler"
+)
+
+func main() {
+	// Create a new gRPC server
+	grpcServer := grpc.NewServer()
+
+	// Create a new connection pool
+	var conn []*handler.Connection
+
+	pool := &handler.Pool{
+		Connection: conn,
+	}
+
+	// Register the pool with the gRPC server
+	proto.RegisterBroadcastServer(grpcServer, pool)
+
+	// Create a TCP listener at port 8080
+	listener, err := net.Listen("tcp", ":8080")
+
+	if err != nil {
+		log.Fatalf("Error creating the server %v", err)
+	}
+
+	fmt.Println("Server started at port :8080")
+
+	// Start serving requests at port 8080
+	if err := grpcServer.Serve(listener); err != nil {
+		log.Fatalf("Error creating the server %v", err)
+	}
+}
