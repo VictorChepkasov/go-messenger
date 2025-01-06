934bf1414c799e06cdf9ab20cbb35e24761219a1
Victor Chepkasov <victorsugary@gmail.com>
2025-01-06 19:45:43 +0500

    change structure

--- go.mod
+++ go.mod
@@ -3,14 +3,11 @@ module messenger
 go 1.23.2
 
 require (
-	github.com/VictorChepkasov/protos v0.0.0-20250104174026-c0b12346ccf7
-	google.golang.org/grpc v1.69.2
-	google.golang.org/protobuf v1.36.1
-)
-
-require (
-	golang.org/x/net v0.30.0 // indirect
-	golang.org/x/sys v0.26.0 // indirect
-	golang.org/x/text v0.19.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20241015192408-796eee8c2d53 // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
+	golang.org/x/net v0.33.0 // indirect
+	golang.org/x/sys v0.29.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250102185135-69823020774d // indirect
+	google.golang.org/grpc v1.69.2 // indirect
+	google.golang.org/protobuf v1.36.1 // indirect
 )
--- internal/services/server.go
+++ internal/services/server.go
@@ -5,33 +5,36 @@ import (
 	"log"
 	"net"
 
+	"github.com/VictorChepkasov/protos/messenger"
 	pb "github.com/VictorChepkasov/protos/messenger"
 
 	"google.golang.org/grpc"
-	"google.golang.org/protobuf/types/known/emptypb"
 )
 
 type server struct {
 	pb.UnimplementedMessengerServer
-	messages []pb.Message
+	messages []*pb.Message
 }
 
-func (s server) SendMessage(ctx context.Context, msg pb.Message) (pb.Message, error) {
+func (s server) SendMessage(ctx context.Context, msg *pb.Message) (*pb.Message, error) {
 	s.messages = append(s.messages, msg)
 	return msg, nil
 }
 
-func (s server) ReceiveMessages(empty emptypb.Empty, stream pb.Messenger_ReceiveMessagesServer) error {
+func (s server) ReceiveMessages(empty *messenger.Empty, stream pb.Messenger_ReceiveMessagesServer) error {
 	for _, msg := range s.messages {
-		if err := stream.Send(&msg); err != nil {
+		if err := stream.Send(msg); err != nil {
+			log.Fatalf("could not receive messages: %v", err)
 			return err
 		}
 	}
 	return nil
 }
 
+func (s server) mustEmbedUnimplementedMessengerServer() {}
+
 func main() {
-	lis, err := net.Listen("tcp", ":50051")
+	lis, err := net.Listen("tcp", ":5051")
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
@@ -39,7 +42,7 @@ func main() {
 	grpcServer := grpc.NewServer()
 	pb.RegisterMessengerServer(grpcServer, &server{})
 
-	log.Println("Server is running on port 50051...")
+	log.Println("Server is running on port 5051...")
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
